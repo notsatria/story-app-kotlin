7bd791760c50092c24bd4b001f1bb1a5778f4cb7
notsatria <buanasatriaa@gmail.com>
2024-05-07 16:12:21 +0700

    initial: initialize theme and Splash Activity

---
+++ app/src/main/java/com/notsatria/storyapp/ui/SplashActivity.kt
@@ -0,0 +1,30 @@
+package com.notsatria.storyapp.ui
+
+import android.os.Bundle
+import android.os.Handler
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.notsatria.storyapp.R
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_splash)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        CoroutineScope(Dispatchers.Main).launch {
+            delay(2000)
+        }
+    }
+}
