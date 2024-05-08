9815064170216e9080b1e12c48d32ebdecb48ce3
notsatria <buanasatriaa@gmail.com>
2024-05-08 21:14:46 +0700

    feat: add bottom navbar

---
+++ app/src/main/java/com/notsatria/storyapp/ui/main/ui/dashboard/AddStoryFragment.kt
@@ -0,0 +1,42 @@
+package com.notsatria.storyapp.ui.main.ui.dashboard
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import com.notsatria.storyapp.databinding.FragmentAddStoryBinding
+
+class AddStoryFragment : Fragment() {
+
+    private var _binding: FragmentAddStoryBinding? = null
+
+    // This property is only valid between onCreateView and
+    // onDestroyView.
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        val addStoryViewModel =
+            ViewModelProvider(this).get(AddStoryViewModel::class.java)
+
+        _binding = FragmentAddStoryBinding.inflate(inflater, container, false)
+        val root: View = binding.root
+
+        val textView: TextView = binding.textDashboard
+        addStoryViewModel.text.observe(viewLifecycleOwner) {
+            textView.text = it
+        }
+        return root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
