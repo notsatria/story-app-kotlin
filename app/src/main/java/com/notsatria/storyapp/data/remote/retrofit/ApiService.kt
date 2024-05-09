9d868e01162ef7d885b49fe1b709b9e4d69e8775
notsatria <buanasatriaa@gmail.com>
2024-05-09 22:39:17 +0700

    feat: create ApiService ApiResponse and UserLoginResponse

--- app/src/main/java/com/notsatria/storyapp/data/remote/retrofit/ApiService.kt
+++ app/src/main/java/com/notsatria/storyapp/data/remote/retrofit/ApiService.kt
@@ -1,6 +1,15 @@
 package com.notsatria.storyapp.data.remote.retrofit
 
-import retrofit2.http.GET
+import com.notsatria.storyapp.data.remote.response.ApiResponse
+import com.notsatria.storyapp.data.remote.response.UserLoginResponse
+import retrofit2.Call
+import retrofit2.http.POST
 
 interface ApiService {
+    @POST("register")
+    fun register(name: String, email: String, password: String): Call<ApiResponse>
+
+    @POST("login")
+    fun login(name: String, email: String, password: String): Call<UserLoginResponse>
+
 }
